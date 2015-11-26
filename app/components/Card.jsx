07a06403024422f23fce979710fd826f6462b423
Ken Ding <aenkil@gmail.com>
2015-11-27 01:07:35 +1100

    Additional styling to the button + overlay.

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -6,10 +6,42 @@ const cx = classNames.bind(styles);
 
 export default class Card extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHover: false
+    };
+    
+    this.handleMouseOver = this.handleMouseOver.bind(this);
+    this.handleMouseOut = this.handleMouseOut.bind(this);
+  }
+
+  handleMouseOver(evt) {
+    this.setState({
+      isHover: true
+    });
+  }
+
+  handleMouseOut(evt) {
+    this.setState({
+      isHover: false
+    });
+  }
+
   render() {
     const { price, agency, mainImage } = this.props;
+    const { isHover } = this.state;
     return (
-      <div className={cx('card', 'card--gray-theme')}>
+      <div className={cx(
+        'card', 
+        'card--gray-theme')}
+          onMouseOver={this.handleMouseOver}
+          onMouseOut={this.handleMouseOut}>
+        <div className={cx('card__overlay', {
+          'card__overlay--visible': isHover
+        })}>
+          <button onClick={evt=> { console.log(evt) }} className={cx('card__button')}>Add Property</button>
+        </div>
         <div style={ { backgroundColor: agency.brandingColors.primary }}>
           <img className={cx('card__agency-logo')} src={agency.logo}/>
         </div>
