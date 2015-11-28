75fb77b8a2b6fa89a3944b6b4343e52884627bae
Ken Ding <aenkil@gmail.com>
2015-11-28 17:39:14 +1100

    Removing postinstalll as not needed. Configured the Heroku env to be development for easy deploy.

--- devServer.js
+++ devServer.js
@@ -23,7 +23,7 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(process.env.PORT || 3000, 'localhost', function(err) {
+app.listen(process.env.PORT || 3000, function(err) {
   if (err) {
     console.log(err);
     return;
