5fddc50c1d1acb22522544c0ad759546c312dda5
Ken Ding <aenkil@gmail.com>
2015-11-28 16:46:44 +1100

    Add Procfile for Heroku deploy

--- devServer.js
+++ devServer.js
@@ -23,7 +23,7 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, 'localhost', function(err) {
+app.listen(process.env.PORT || 3000, 'localhost', function(err) {
   if (err) {
     console.log(err);
     return;
